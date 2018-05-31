ddd5f6ce79e67cbc3f1a0454202b7aef53a13d50
Slawwomir <slawekzpl@gmail.com>
2018-05-31 19:42:41 +0200

    Player class added

---
+++ src/sample/Server/Player.java
@@ -0,0 +1,26 @@
+package sample.Server;
+
+import javafx.geometry.Pos;
+
+public class Player {
+    private Position position;
+
+}
+
+final class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public int getX() {
+        return x;
+    }
+}
